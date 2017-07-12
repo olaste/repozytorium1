61d343ffa68742d41893b6498efc61d4a80ee28a
Ola <alexandrastecz@yahoo.pl>
2017-07-12 20:30:44 +0200

    Dodanie klasy Klasa

---
+++ src/pl/sdacademy/streams/Klasa.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.streams;
+
+/**
+ * Created by RENT on 2017-07-12.
+ */
+public class Klasa {
+    private int a;
+
+    public Klasa(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
